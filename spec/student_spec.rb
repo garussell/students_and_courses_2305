f173462339f2fda0d04bdaadb5d71a2c66dafd17
Allen Russell <garyrusselljr@gmail.com>
2023-05-24 09:09:54 -0600

    test: add test for log_scores

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -14,4 +14,13 @@ RSpec.describe Student do
       expect(@student.scores).to eq []
     end
   end
+
+  describe "#log_scores(score)" do
+    it "can log scores" do
+      @student.log_score(89)
+      @student.log_score(78)
+
+      expect(@student.scores).to eq([89, 78])
+    end
+  end
 end
