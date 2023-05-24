9bc957bd59cbdadef5cb4bb919ea9e6bee5b2389
Allen Russell <garyrusselljr@gmail.com>
2023-05-24 09:15:01 -0600

    feat: add grade method

--- lib/student.rb
+++ lib/student.rb
@@ -12,4 +12,12 @@ class Student
   def log_score(score)
     @scores << score
   end
+
+  def grade
+    sum = 0
+    @scores.each do |score|
+      sum += score
+    end
+    sum.to_f / @scores.count
+  end
 end
