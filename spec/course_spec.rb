b4880d511573ce55098203f16eb2884595eb5709
Allen Russell <garyrusselljr@gmail.com>
2023-05-24 09:17:43 -0600

    test: add test for initialize

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,5 +3,18 @@ require './lib/course'
 require './lib/student'
 
 RSpec.describe Course do 
+  before(:each) do
+    @course = Course.new("Calculus", 2) 
+    @student1 = Student.new({name: "Morgan", age: 21})
+    @student2 = Student.new({name: "Jordan", age: 29}) 
+  end
 
+  describe "#exists" do
+    it "exists and has attributes" do
+      expect(@course).to be_an_instance_of(Course)
+      expect(@course.name).to eq("Calculus")
+      expect(@course.capacity).to eq(2)
+      expect(@course.students).to eq []
+    end
+  end
 end
