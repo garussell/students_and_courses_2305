835d26e9720754964636ae5402101154dc874579
Allen Russell <garyrusselljr@gmail.com>
2023-05-24 09:41:41 -0600

    feat: add constructor with attributes

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,3 +1,11 @@
-class Gradebook
+class GradeBook
+  attr_reader :instructor,
+              :courses
+
+  def initialize(instructor)
+    @instructor = instructor
+    @courses = []
+  end
+
   
 end
