d02887ad5b61961be34f38f160e74a4abbc52f2d
Allen Russell <garyrusselljr@gmail.com>
2023-05-24 09:12:04 -0600

    test: add test for grade method

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -23,4 +23,13 @@ RSpec.describe Student do
       expect(@student.scores).to eq([89, 78])
     end
   end
+
+  describe "#grade" do
+    it "can show an average of all the scores" do
+      @student.log_score(89)
+      @student.log_score(78)
+
+      expect(@student.grade).to eq(83.5)
+    end
+  end
 end
