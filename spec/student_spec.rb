015f256dc71bbfdb9f6483e0d1acbf20700c3e11
Allen Russell <garyrusselljr@gmail.com>
2023-05-24 09:08:05 -0600

    test: add test for initialize

--- spec/student_spec.rb
+++ spec/student_spec.rb
@@ -2,5 +2,16 @@ require 'rspec'
 require './lib/student'
 
 RSpec.describe Student do
-  
+  before(:each) do
+    @student = Student.new({name: "Morgan", age: 21}) 
+  end
+
+  describe "#exists" do
+    it "exists and has attributes" do
+      expect(@student).to be_an_instance_of(Student)
+      expect(@student.name).to eq("Morgan")
+      expect(@student.age).to eq(21)
+      expect(@student.scores).to eq []
+    end
+  end
 end
